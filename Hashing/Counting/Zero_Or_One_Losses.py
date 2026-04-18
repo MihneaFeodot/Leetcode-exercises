aec11090f81f9adcf503c975fae4943ec5a2031e
MihneaFeodot <mihnea.feodot@stud.acs.upb.ro>
2026-04-18 23:05:24 +0300

    Added unfinished problem

---
+++ Hashing/Counting/Zero_Or_One_Losses.py
@@ -0,0 +1,16 @@
+'''
+Input: matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+Output: [[1,2,10],[4,5,7,8]]
+Explanation:
+Players 1, 2, and 10 have not lost any matches.
+Players 4, 5, 7, and 8 each have lost one match.
+Players 3, 6, and 9 each have lost two matches.
+Thus, answer[0] = [1,2,10] and answer[1] = [4,5,7,8].
+'''
+
+
+from collections import defaultdict
+
+class Solution:
+    def findWinners(self, matches: list[list[int]]) -> list[list[int]]:
+        
